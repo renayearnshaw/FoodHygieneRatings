8b18e6875487e4fea85c14bb9d0e6b833d853d05
Renay Earnshaw <renay.earnshaw@sky.uk>
2018-04-11 14:01:17 +0100

    Added UriComponentBuilder to Establishments service

--- src/main/java/ratings/config/EstablishmentsConfig.java
+++ src/main/java/ratings/config/EstablishmentsConfig.java
@@ -4,20 +4,37 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import ratings.services.EstablishmentsService;
 import ratings.services.EstablishmentsServiceImpl;
 
 @Configuration
 public class EstablishmentsConfig {
-    @Value(value = "${establishments.url}")
-    private String uri;
     @Value(value = "${headers.api_version.key}")
     private String apiVersionKey;
     @Value(value = "${headers.api_version.value}")
     private String apiVersionValue;
+    @Value(value = "${uri.scheme}")
+    private String scheme;
+    @Value(value = "${uri.host}")
+    private String host;
+    @Value(value = "${establishments.uri.path}")
+    private String path;
+    @Value(value = "${establishments.uri.idQueryParam}")
+    private String idQueryParam;
+    @Value(value = "${establishments.uri.pageSizeQueryParam}")
+    private String pageSizeQueryParam;
 
     @Bean
     EstablishmentsService establishmentsService(RestTemplate restTemplate) {
-        return new EstablishmentsServiceImpl(apiVersionKey, apiVersionValue, uri, restTemplate);
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .newInstance()
+                .scheme(scheme)
+                .host(host)
+                .path(path)
+                .query(idQueryParam)
+                .query(pageSizeQueryParam);
+
+        return new EstablishmentsServiceImpl(apiVersionKey, apiVersionValue, builder, restTemplate);
     }
 }
--- src/main/java/ratings/services/EstablishmentsServiceImpl.java
+++ src/main/java/ratings/services/EstablishmentsServiceImpl.java
@@ -8,17 +8,18 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+import org.springframework.web.util.UriComponentsBuilder;
 import ratings.model.Establishment;
 import ratings.model.EstablishmentResponse;
 
 public class EstablishmentsServiceImpl implements EstablishmentsService {
 	private final HttpHeaders headers = new HttpHeaders();
-	private final String uri;
+	private final UriComponentsBuilder builder;
 	private final RestTemplate restTemplate;
 
-	public EstablishmentsServiceImpl(String apiVersionKey, String apiVersionValue, String uri, RestTemplate restTemplate) {
+	public EstablishmentsServiceImpl(String apiVersionKey, String apiVersionValue, UriComponentsBuilder builder, RestTemplate restTemplate) {
 		headers.set(apiVersionKey, apiVersionValue);
-		this.uri = uri;
+		this.builder = builder;
 		this.restTemplate = restTemplate;
 	}
 
@@ -26,7 +27,7 @@ public class EstablishmentsServiceImpl implements EstablishmentsService {
 	@Override
 	public List<Establishment> getEstablishmentsInAuthority(long authorityId) {
         ResponseEntity<EstablishmentResponse> response = restTemplate.exchange(
-				String.format(uri, authorityId), 
+				builder.buildAndExpand(authorityId).toString(),
 				HttpMethod.GET,
 				new HttpEntity<>(headers),
 				EstablishmentResponse.class);
